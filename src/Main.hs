4fabcc5fc33400ed5e6d326fc57e40b55f4578ce
Matt Noonan <matt.noonan@gmail.com>
2015-09-15 22:30:37 -0400

    Nicer sample color scheme.

--- src/Main.hs
+++ src/Main.hs
@@ -11,5 +11,5 @@ import UI.WX.Viewer
 
 main :: IO ()
 main = wxView (runComplexDynamics rabbit) checkers
-    where checkers = checker (grad yellow) (grad blue)
-          grad c = gradient 5 (solid $ dark c) (solid $ light c)
+    where checkers = checker (grad yellow white) (grad (dark blue) blue)
+          grad c1 c2 = gradient 5 (solid c1) (solid c2)
