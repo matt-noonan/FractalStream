0a9f297c0ebde22957970a9be1f60bec7707aac4
Matt Noonan <matt.noonan@gmail.com>
2015-09-20 21:15:37 -0400

    Add some OS X-specific things to .gitignore and Setup.hs

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,16 @@
+import Distribution.MacOSX
 import Distribution.Simple
-main = defaultMain
+
+main :: IO ()
+main = defaultMainWithHooks $ simpleUserHooks {
+         postBuild = appBundleBuildHook guiApps -- no-op if not MacOS X
+       }
+
+guiApps :: [MacApp]
+guiApps = [MacApp "FractalStream"
+                  Nothing
+                  Nothing -- Build a default Info.plist for the icon.
+                  [] -- No other resources.
+                  [] -- No other binaries.
+                  ChaseWithDefaults
+          ]
