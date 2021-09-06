118ae19a1c47274b556ccfceb68b48ae16ed73b6
Matt Noonan <mnoonan@noether.lan>
2021-09-06 13:19:36 -0400

    Split into subprojects

---
+++ fractalstream-core/src/Task/Block.hs
@@ -0,0 +1,152 @@
+{-# options_ghc -Wno-type-defaults #-}
+
+{- |
+Module      : Task.Block
+Description : Blocks encapsulate the task of rendering a picture of a
+              dynamical system to one or more tiles.
+-}
+module Task.Block
+  ( Block(..)
+  , fillBlock
+  , progressively
+  ) where
+
+import Color.Color
+
+import Task.Concurrent
+
+import Control.Concurrent.MVar
+import Control.Monad
+import Data.Word
+--import Foreign.Ptr
+import Foreign.ForeignPtr
+import Control.Concurrent
+import Data.Array.IO hiding (index)
+import System.Random
+import Data.Time (diffUTCTime, getCurrentTime)
+
+import Utilities (groupsOf)
+
+-- | A Block carries the information required to go from a
+--   runnable dynamical system and choice of color scheme
+--   to a buffer filled with the resulting color data.
+data Block =
+  Block { coordToModel  :: (Double, Double) -> (Double, Double)
+        -- , compute       :: Word32 -> Ptr Double -> Ptr Color -> IO ()
+        , compute       :: [(Double, Double)] -> IO [Color]
+        , logSampleRate :: Int
+          -- ^ The rate of over- or under-sampling to use.
+          --      * logSampleRate == 0: draw one point per pixel.
+          --      * logSampleRate == N < 0: draw 2^-N by 2^-N pixel
+          --          blocks per model point.
+          --      * logSampleRate == N > 0: subsample each pixel
+          --          on a 2^N by 2^N subgrid and average the
+          --          results, for a smoother picture.
+        , blockBuffer   :: Synchronizable (ForeignPtr Word8)
+          -- ^ The pixel buffer to write into.
+        , xStride       :: Int
+          -- ^ The width of the pixel buffer.
+        , x0            :: Int
+          -- ^ The upper-left x coordinate of this block in the pixel buffer.
+        , y0            :: Int
+          -- ^ The upper-left y coordinate of this block in the pixel buffer.
+        , xSize         :: Int
+          -- ^ The width of the block.
+        , ySize         :: Int
+          -- ^ The height of the block
+        , shouldRedraw  :: MVar ()
+          -- ^ A variable used to signal that this block is complete
+          --   and the pixel buffer should be redrawn.
+        }
+
+-- | Render the block into its output buffer.
+fillBlock :: Block -> IO ()
+fillBlock Block{..} = do
+    let skip = if logSampleRate < 0 then 2^(negate logSampleRate) else 1
+        k    = if logSampleRate > 0 then 2^logSampleRate          else 1
+        subsamples  = [s / k | s <- [0..k-1]]
+        nSubsamples = floor (k^2)
+
+    let points    = [ (x,y) | y <- [0, skip .. ySize - 1]
+                            , x <- [0, skip .. xSize - 1] ]
+        uv_points = [ (fromIntegral (x0 + x), fromIntegral (y0 + y))
+                    | (x,y) <- points ]
+        indexOf (u,v) = u + v * xStride
+
+    let subsample (u,v) = [ (u + du, v + dv) | du <- subsamples, dv <- subsamples ]
+
+    let samples = map coordToModel $ concatMap subsample uv_points
+
+    -- Run the computation on each subsampled point.
+    results <- compute samples
+
+    -- Resample the results
+    let rgbs = resampleBy averageColor nSubsamples results
+
+    -- Fill target buffer with result colors.
+    with blockBuffer $ \buffer -> withForeignPtr buffer $ \ptr -> do
+      forM_ (zip uv_points rgbs) $ \((u,v), rgb) -> do
+        let index = floor $ u + v * fromIntegral xStride
+        forM_ [indexOf (du,dv) | dv <- [0 .. skip - 1]
+                               , du <- [0 .. skip - 1] ] $ \offset -> do
+          pokeColor ptr (index + offset) rgb
+
+    -- Completed the block, signal for a redraw
+    void (tryPutMVar shouldRedraw ())
+
+resampleBy :: ([a] -> b) -> Int -> [a] -> [b]
+resampleBy f n
+  | n > 0     = map f . groupsOf n
+  | otherwise = map f . groupsOf 1
+
+
+-- | Chop up a block into sub-blocks, delegate rendering
+--   tasks for sub-blocks, and blit the results back
+--   into an image.
+progressively :: (Block -> IO ()) -> (Block -> IO ())
+
+progressively render block = do
+
+    let subBlockSize = 8
+        width   = xSize block
+        height  = ySize block
+        xBlocks =  case width `divMod` subBlockSize of
+            (z, 0) -> z
+            (z, _) -> z -- + 1
+        yBlocks = case height `divMod` subBlockSize of
+            (z, 0) -> z
+            (z, _) -> z -- + 1
+
+    blockIDs <- shuffle [(x,y) | x <- [0..xBlocks - 1], y <- [0..yBlocks - 1]]
+
+    poolSize <- subtract 1 <$> getNumCapabilities
+    caps <- getNumCapabilities
+    putStrLn $ show caps ++ " capabilities, pool size " ++ show poolSize ++ " (w=" ++ show (xSize block) ++ ", h=" ++ show (ySize block) ++ ")"
+
+    let rates = filter (<= logSampleRate block) [-4, -2, 0, logSampleRate block]
+
+    let todo = [(rate, x, y) | rate <- rates, (x,y) <- blockIDs]
+    putStrLn $ "***** start @ rates=" ++ show rates
+    start <- getCurrentTime
+    forPool_ poolSize todo $ \(rate,x,y) -> do
+      render $ block { xSize = subBlockSize, ySize = subBlockSize,
+                       x0 = subBlockSize * x, y0 = subBlockSize * y,
+                       logSampleRate = rate }
+    end <- getCurrentTime
+    putStrLn $ "***** " ++ show width ++ " x " ++ show height ++ " @ rates " ++ show rates
+               ++ " rendered in " ++ show (diffUTCTime end start)
+
+shuffle :: [a] -> IO [a]
+shuffle xs = do
+        ar <- makeArray xs
+        forM_ [2..n] $ \i -> do
+            j <- randomRIO (1,i)
+            vi <- readArray ar i
+            vj <- readArray ar j
+            writeArray ar j vi
+            writeArray ar i vj
+        forM [1..n] (readArray ar)
+  where
+    n = length xs
+    makeArray :: [a] -> IO (IOArray Int a)
+    makeArray = newListArray (1,n)
---
+++ fractalstream-core/src/Task/Concurrent.hs
@@ -0,0 +1,124 @@
+{- |
+Module      : Task.Concurrent
+Description : Utilities for concurrent operations
+-}
+
+module Task.Concurrent
+       ( forPool
+       , forPool_
+       , Synchronizable()
+       , synchronized
+       , synchronizedTo
+       , with
+       , synchedWith
+       ) where
+
+import           Control.Concurrent
+import           Control.Concurrent.Async
+import           Control.Monad
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as VM
+
+-- | forPool n is similar to forM, except the actions
+--   are executed concurrently by a pool of n workers.
+forPool :: Int -> [a] -> (a -> IO b) -> IO [b]
+forPool nSimul xs action = do
+  result <- VM.new (length xs)
+  forPool_ nSimul (zip [0..] xs) $ \(i,x) -> do
+    y <- action x
+    VM.unsafeWrite result i y
+  V.toList <$> V.unsafeFreeze result
+
+-- | forPool_ n is similar to forM_, except the actions
+--   are executed concurrently by a pool of n workers.
+forPool_ :: Int -> [a] -> (a -> IO ()) -> IO ()
+forPool_ nSimul xs action
+  | nSimul < 1 = forPool_ 1 xs action
+  | otherwise = do
+      todo <- newMVar xs
+      let loop = do
+            -- pop a work item
+            next <- modifyMVar todo $ \case
+              []      -> pure ([], Nothing)
+              (x:xs') -> pure (xs', Just x)
+            -- process the item and loop
+            case next of
+              Nothing -> pure ()
+              Just x  -> action x >> loop
+      forConcurrently_ [1..nSimul] (const loop)
+
+-- | A resource on which actions may be performed
+--   by many threads concurrently, or serially
+--   by waiting for completion of current actions
+--   before allowing more actions on the resource
+--   to execute.
+data Synchronizable a = Synchronizable
+  { available :: MVar ()
+  , users     :: MVar Int
+  , finished  :: MVar ()
+  , resource  :: a
+  }
+
+-- | Create a new synchronization structure
+--   around a shared resource.
+synchronized :: a -> IO (Synchronizable a)
+synchronized res = do
+  _available <- newMVar ()
+  _users     <- newMVar 0
+  _finished  <- newMVar ()
+  return $ Synchronizable { available = _available
+                          , users     = _users
+                          , finished  = _finished
+                          , resource  = res
+                          }
+
+-- | Use an existing synchronized resource to
+--   control access to another resource.
+synchronizedTo :: a -> Synchronizable b -> Synchronizable a
+synchronizedTo res obj = obj { resource = res }
+
+-- | Execute an action on a synchronized resource, perhaps
+--   concurrently with other threads.
+with :: Synchronizable a -> (a -> IO b) -> IO b
+with obj action = do
+  let usrs = users obj
+  -- Block until general access is allowed.
+  _ <- takeMVar (available obj)
+
+  -- Increment the number of active users, and
+  -- flag the resource as not finished if we are
+  -- the first worker.
+  takeMVar usrs >>= \n -> do
+      when (n == 0) $ void $ tryTakeMVar (finished obj)
+      putMVar usrs (n + 1)
+
+  putMVar (available obj) ()
+
+  -- Perform the action
+  result <- action $ resource obj
+
+  -- Decrement the number of users and, if we were the last
+  -- active user, flag the resource as finished.
+  takeMVar usrs >>= \n -> do
+      when (n == 1) $ void $ tryPutMVar (finished obj) ()
+      putMVar usrs (n - 1)
+
+  return result
+
+-- | Execute an action on a synchronized resource while
+--   preventing concurrent access to the resource.  synchedWith
+--   will wait until any running actions complete before
+--   executing the given action, and will prevent new actions
+--   from beginning.
+synchedWith :: Synchronizable a -> (a -> IO b) -> IO b
+synchedWith obj action = do
+  -- Prevent anybody else from beginning new work with this resource.
+  void $ takeMVar (available obj)
+
+  -- Wait until there are no users of the resource, then run the action.
+  readMVar (finished obj)
+  result <- action (resource obj)
+
+  -- Allow others to access the resource again.
+  putMVar (available obj) ()
+  return result
