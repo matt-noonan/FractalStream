2eb17af54b31f761e61cb4b2b101309797525065
Vinicius Ambrosi <42151837+vambrosi@users.noreply.github.com>
2026-03-03 20:01:34 -0800

    Add non-monadic indexed fold

--- fractalstream-core/src/Data/Indexed/Functor.hs
+++ fractalstream-core/src/Data/Indexed/Functor.hs
@@ -217,14 +217,14 @@ indexedUnfoldM f =
 
 -- | Perform an @indexedFold@, but also make the original sub-ASTs
 -- available during the fold.
--- indexedFoldWithOriginal
---   :: forall a f i
---    . IFunctor f
---   => (forall j. f (FIX f :*: a) j -> Eval (a j))
---   -> Eval (FIX f i)
---   -> Eval (a i)
--- indexedFoldWithOriginal f =
---   snd . indexedFold (\x -> ((imap (const fst) x), f x))
+indexedFoldWithOriginal
+  :: forall a f i
+   . IFunctor f
+  => (forall j. f (FIX f :*: a) j -> Eval (a j))
+  -> Eval (FIX f i)
+  -> Eval (a i)
+indexedFoldWithOriginal f =
+  snd . indexedFold (\x -> ((imap (const fst) x), f x))
 
 indexedFoldWithOriginalM
   :: forall a f i m
