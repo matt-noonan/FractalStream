8d15d1045de59dfd7ef549bb2931afaac1e0851e
Matt Noonan <matt.noonan@gmail.com>
2026-08-04 17:27:11 -0400

    Here you go, Dierk!

---
+++ examples/mandelbrot-default.fs
@@ -0,0 +1,77 @@
+configuration:
+  size: 400x200
+  title: Configuration
+  vertical-contents:
+  - text-entry:
+      label: 'Selected value of C:'
+      type: C
+      value: "-0.16406250000000 - 0.12500000000000\U0001D456"
+      variable: C
+  - text-entry:
+      label: 'Max. iterations: '
+      type: Z
+      value: 100
+      variable: maxIters
+  - text-entry:
+      label: 'Max. radius: '
+      type: R
+      value: 10
+      variable: maxRadius
+setup:
+  size: 200x200
+  title: Parametric complex dynamics
+  vertical-contents:
+  - text-entry:
+      environment:
+        C: ℂ
+        z: ℂ
+      label: 'Iterate f(z) = '
+      type: C
+      value: z^2 + C
+      variable: f
+  - text-entry:
+      environment:
+        C: ℂ
+        maxRadius: ℝ
+        z: ℂ
+      label: until
+      type: Boolean
+      value: '|z| > maxRadius'
+      variable: stop
+viewers:
+- code: "z : C <- 0\niterate z -> «f» until «stop»\n\nif «stop»:\n   color <- rainbow(iterations/20)\n
+    \ \nelse:\n  color <- black\n"
+  escape-radius: maxRadius
+  iteration-limit: maxIters
+  position: 2238x348
+  size: 512x512
+  title: Parameter plane
+  z-coord: C
+- code: |
+    iterate z -> «f» until «stop»
+
+    if «stop»:
+       color <- rainbow(iterations/20)
+    else:
+      color <- black
+  escape-radius: maxRadius
+  iteration-limit: maxIters
+  position: 2797x341
+  size: 512x512
+  title: Dynamical plane
+  tools:
+  - actions:
+    - code: erase
+      event: deactivated
+    - code: |
+        prevZ : C <- z
+        erase
+        until «stop»:
+          prevZ <- z
+          draw point at z
+          z <- «f»
+          draw line from z to prevZ
+      event: click-or-drag
+    name: Trace
+    shortcut: t
+  z-coord: z
