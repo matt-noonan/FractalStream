ffaae3abc3326fa1de19b49cbb8e00190024aba8
Matt Noonan <matt.noonan@gmail.com>
2026-09-11 13:23:29 -0400

    Revert "Add allocator (#84)" (#86)
    This reverts commit e0fe42eac7f853419575b5f53ee9d605d7b81dd6.

--- fractalstream-backend-pure/src/Backend.hs
+++ fractalstream-backend-pure/src/Backend.hs
@@ -1,15 +1,10 @@
 module Backend
   ( withBackend
-  , module Actor.Viewer   -- re-export Backend, ToolRunnerFactory, etc.
   ) where
 
 import Actor.Viewer
 import Backend.Pure
 
-withBackend :: (Backend -> IO a) -> IO a
+withBackend :: (ViewerCompiler -> IO a) -> IO a
 withBackend action =
-  action Backend
-    { bViewerCompiler    = ViewerCompiler interpretViewer
-    , bToolRunnerFactory = defaultToolRunnerFactory
-    , bToolRunner        = defaultToolRunner
-    }
+  action (ViewerCompiler interpretViewer)
